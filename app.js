7e9d1d322bff7f0a93de82f1314d53b5f6e5d3cf
samade747 <samad.e747@gmail.com>
2023-11-02 22:20:12 +0500

    6516515

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let getweather = () => {
     let Api_key = 'a19ac5ed42c436634a170333b3d18673'
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${c_name}&units=metric&appid=${Api_key}`)
     .then((response)=>{
-       let a = document.querySelector('#temp').innerHTML = res.data.main.temp
-       let b = document.querySelector('#humidity').innerHTML = res.data.main.humidity
-       let c = document.querySelector('#temp_max').innerHTML = res.data.main.temp_max
-       let d = document.querySelector('#temp_min').innerHTML = res.data.main.temp_min        
+       document.querySelector('#temp').innerHTML     = 'Temperature is:'  + response.data.main.temp
+       document.querySelector('#humidity').innerHTML = 'Humidity is:'  + response.data.main.humidity
+       document.querySelector('#temp_max').innerHTML = 'Temp max is:'  + response.data.main.temp_max
+       document.querySelector('#temp_min').innerHTML = 'Temp low is:'  + response.data.main.temp_min        
     })
     .catch((error)=>{
         console.log(error)
