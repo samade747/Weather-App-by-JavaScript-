08876b8ed05859a756c9fce98cf8e7964c7eb1de
samade747 <samad.e747@gmail.com>
2023-11-02 20:54:51 +0500

    15153

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 let getweather = () => {
     let Api_key = 'a19ac5ed42c436634a170333b3d18673'
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=${Api_key}`)
-    .then((Response)=>{
-        console.log(Response)
+    .then((response)=>{
+        console.log(response)
     })
     .catch((error)=>{
         console.log(error)
 
-    })    
-
-
-
-
+    })       
 
 }
