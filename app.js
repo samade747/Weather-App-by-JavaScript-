f10efeb3e44c1c432a42df2e70149ee2b810b819
samade747 <samad.e747@gmail.com>
2023-11-02 21:05:09 +0500

    455457

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let getweather = () => {
+    let c_name = document.querySelector("#inp-weather").value
     let Api_key = 'a19ac5ed42c436634a170333b3d18673'
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=${Api_key}`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${c_name}&units=metric&appid=${Api_key}`)
     .then((response)=>{
-        console.log(response)
+        console.log(response.data.main.temp)
     })
     .catch((error)=>{
         console.log(error)
